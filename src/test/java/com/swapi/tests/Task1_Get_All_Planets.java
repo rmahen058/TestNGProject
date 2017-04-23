c796eb21cf160801c6fc282e4409ea109dc9f0cb
rakes <rakes@LAPTOP-5VLC0MFK>
2017-04-23 01:58:37 -0500

    Updated code with additional scenarios.

--- src/test/java/com/swapi/tests/Task1_Get_All_Planets.java
+++ src/test/java/com/swapi/tests/Task1_Get_All_Planets.java
@@ -1,6 +1,12 @@
 package com.swapi.tests;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.ParseException;
@@ -9,35 +15,77 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import org.testng.internal.junit.ArrayAsserts;
 
+import com.swapi.dto.Planet;
+import com.swapi.dto.Residents;
 import com.swapi.utilities.RestAPITester;
 
+/**
+ * @author rakesh
+ * This class is created to hold all the test cases which consumes 
+ * SWAPI for testing Get All Planets *
+ */
+/**
+ * @author rakes
+ *
+ */
+/**
+ * @author rakes
+ *
+ */
 public class Task1_Get_All_Planets extends RestAPITester {
 	public static boolean reporterFlag = true;
 	public static HttpResponse respGETService;
 	public static int respCode;
 
+
+	/**
+	 * This method confirms SWAPI is up and running
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	@Test
 	public static void Auto_SWAPI_GET_Base_Test_001() throws ClientProtocolException, IOException {
+		Reporter.log("--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
+		
 		respCode = getServiceStatusCode("http://swapi.co/api");
 		Reporter.log("HttpResponse Status Code of  BaseTest: " + respCode, reporterFlag); // We can use log4J for logging
 		Assert.assertEquals(respCode, HttpStatus.SC_OK);
 	}
 
+	/**
+	 * This method consumes SWAPI for Planet resources
+	 * and validates Http Status Code: 200
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
 	@Test(priority = 1, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
 	public static void Auto_SWAPI_GET_ALL_Test_002() throws ClientProtocolException, IOException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
 		respCode = getServiceStatusCode("http://swapi.co/api/planets");
 		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag); 
 		Assert.assertEquals(respCode, HttpStatus.SC_OK);
 	}
 
+	/**
+	 * This method consumes SWAPI for Planet resources
+	 * and validates attributes of the planet resources
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws JSONException
+	 */
 	@Test(priority = 2, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
 	public static void Auto_SWAPI_GET_ALL_Test_003()
 			throws ClientProtocolException, IOException, ParseException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
 		String[] expAttributes = { "films", "edited", "created", "climate", "rotation_period", "url", "population",
 				"orbital_period", "surface_water", "diameter", "gravity", "name", "residents", "terrain" };
 
@@ -53,50 +101,101 @@ public class Task1_Get_All_Planets extends RestAPITester {
 			ArrayAsserts.assertArrayEquals(expAttributes, attr);
 			Reporter.log("Attributes for the Resource Plannets: ", reporterFlag);
 			for (int x = 0; x < attr.length; x++) {
-				Reporter.log(attr[x], reporterFlag);
+				Reporter.log("Attribute: " + attr[x], reporterFlag);
 			}
 		}
 		Assert.assertEquals("Content-Type: application/json", respGETService.getEntity().getContentType().toString());
-
 	}
 
-	//@Test(priority = 3, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	
+	/**
+	 * This method consumes SWAPI for Planet resources
+	 * and validates search functionality of the planet resources
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws JSONException
+	 */
+	@Test(priority = 3, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
 	public static void Auto_SWAPI_GET_ALL_Test_004()
 			throws ClientProtocolException, IOException, ParseException, JSONException {
-		String[] expAttributes = { "films", "edited", "created", "climate", "rotation_period", "url", "population",
-				"orbital_period", "surface_water", "diameter", "gravity", "name", "residents", "terrain" };
-
-		Assert.assertEquals(getServiceStatusCode("http://swapi.co/api/planets/?search=al"), HttpStatus.SC_OK);
+		Reporter.log("\n" + "--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
+		
+		String searchString = "rd";
+		Assert.assertEquals(getServiceStatusCode("http://swapi.co/api/planets/?search=" + searchString), HttpStatus.SC_OK);
 
 		respGETService = getServiceContent();
 		JSONObject jo = new JSONObject(EntityUtils.toString(respGETService.getEntity()));
-		int count = jo.getInt("count");
-		Reporter.log("Count for Search: " + count, true);
+		int pCount = jo.getInt("count");
+		Reporter.log("Number of Planets containing character \"" + searchString + "\": " + pCount, true);
+		
 		JSONArray ja = jo.getJSONArray("results");
 		for (int i = 0; i < ja.length(); i++) {
 			String pName = ja.getJSONObject(i).getString("name");
-			System.out.println(pName);
+			Assert.assertTrue(pName.contains(searchString));
+			Reporter.log("Name of the planet: " + pName, reporterFlag);
 		}
-		System.out.println(ja.length());
-
-		for (int j = 0; j < 1; j++) {
-			JSONObject po = ja.getJSONObject(j);
-			String[] attr = JSONObject.getNames(po);
-			ArrayAsserts.assertArrayEquals(expAttributes, attr);
-			for (int x = 0; x < attr.length; x++) {
-				System.out.println("Attributes: " + attr[x]);
-			}
-		}
-
-		Assert.assertEquals("Content-Type: application/json", respGETService.getEntity().getContentType().toString());
-
+		
 	}
 	
 	@Test(priority = 4, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_SWAPI_GET_ALL_Test_005() 
+			throws ClientProtocolException, IOException, ParseException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
+		
+		String strBaseUrl = "http://swapi.co/api/planets/";
+		String strNextElmt = strBaseUrl;
+		List<Planet> lstPlanets = new ArrayList<Planet>();
+		Planet objPlanet;
+
+		do {
+			getServiceStatusCode(strNextElmt);
+			respGETService = getServiceContent();
+			JSONObject jo = new JSONObject(EntityUtils.toString(respGETService.getEntity()));
+
+			strNextElmt = jo.getString("next");
+			JSONArray ja = jo.getJSONArray("results");
+			for (int i = 0; i < ja.length(); i++) {
+				
+				objPlanet = new Planet();
+				
+				objPlanet.setStrPlanetName(ja.getJSONObject(i).getString("name"));
+
+				lstPlanets.add(objPlanet);
+			}
+		} while (!strNextElmt.equalsIgnoreCase("null"));
+		
+		Set<String> set = new HashSet<String>();
+		for(Planet objPlanetTmp : lstPlanets) {
+			set.add(objPlanetTmp.getStrPlanetName());
+			Reporter.log("Planet Name: " + objPlanetTmp.getStrPlanetName(), reporterFlag);
+		}
+		Assert.assertEquals(set.size(), lstPlanets.size());
+		Reporter.log("No duplicate Planet Name found.");
+	}
+
+	
+	/**
+	 * This method consumes SWAPI for Planet resources
+	 * and validates return code is 404 NOT FOUND for invalid
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	@Test(priority = 4, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
 	public static void Auto_SWAPI_GET_ALL_Test_006() throws ClientProtocolException, IOException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
 		respCode = getServiceStatusCode("http://swapi.co/api/plants");
 		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag); 
-		Assert.assertEquals(respCode, HttpStatus.SC_NOT_FOUND);
-		
+		Assert.assertEquals(respCode, HttpStatus.SC_NOT_FOUND);		
 	}
+	
+	
+	/**
+	 * This method is the utility to get the method name
+	 * @return
+	 */
+	public static String getCurrentMethodName() {
+        return Thread.currentThread().getStackTrace()[2].getClassName() + "." + Thread.currentThread().getStackTrace()[2].getMethodName();
+    }	
 }
--- src/test/java/com/swapi/tests/Task1_Get_Planets_By_ID.java
+++ src/test/java/com/swapi/tests/Task1_Get_Planets_By_ID.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
+import org.testng.internal.junit.ArrayAsserts;
+
 import com.swapi.utilities.RestAPITester;
 
 public class Task1_Get_Planets_By_ID extends RestAPITester {
@@ -17,15 +22,140 @@ public class Task1_Get_Planets_By_ID extends RestAPITester {
 
 	@Test
 	public static void Auto_SWAPI_GET_Base_Test_001() throws ClientProtocolException, IOException {
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+				+ "--------------------" + "\n", reporterFlag);
+
 		respCode = getServiceStatusCode("http://swapi.co/api");
-		Reporter.log("HttpResponse Status Code of  BaseTest: " + respCode, reporterFlag); // We can use log4J for logging
+		Reporter.log("HttpResponse Status Code of  BaseTest: " + respCode, reporterFlag);
 		Assert.assertEquals(respCode, HttpStatus.SC_OK);
 	}
 
 	@Test(priority = 1, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_Swapi_GET_ID_TEST_OO1() throws ClientProtocolException, IOException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName() + "--------------------" + "\n", reporterFlag);
+		
+		respCode = getServiceStatusCode("http://swapi.co/api/planets/1/");
+		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag); 
+		Assert.assertEquals(respCode, HttpStatus.SC_OK);
+	}
+		
+	@Test(priority = 2, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
 	public static void Auto_Swapi_GET_ID_TEST_OO2() throws ClientProtocolException, IOException, JSONException {
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+				+ "--------------------" + "\n", reporterFlag);
+
 		respCode = getServiceStatusCode("http://swapi.co/api/planets/99/");
+		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag);
+		Assert.assertEquals(respCode, HttpStatus.SC_NOT_FOUND);
+	}
+	
+	@Test(priority = 3, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_Swapi_GET_ID_TEST_OO3() throws ClientProtocolException, IOException, JSONException {
+		
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+				+ "--------------------" + "\n", reporterFlag);
+		
+		String[] expAttributes = { "films", "edited", "created", "climate", "rotation_period", "url", "population",
+				"orbital_period", "surface_water", "diameter", "gravity", "name", "residents", "terrain" };
+		respCode = getServiceStatusCode("http://swapi.co/api/planets/1/");
+		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag);
+		Assert.assertEquals(respCode, HttpStatus.SC_OK);
+
+		respGETService = getServiceContent();
+		JSONObject jo = new JSONObject(EntityUtils.toString(respGETService.getEntity()));
+		String[] pAttribute = JSONObject.getNames(jo);
+		Reporter.log("Attributes are as below:", reporterFlag);
+		for (int i = 0; i < pAttribute.length; i++) {
+			Reporter.log("Attribute: " + pAttribute[i], reporterFlag);
+		}
+		
+		ArrayAsserts.assertArrayEquals(expAttributes, pAttribute);
+		Assert.assertEquals("Content-Type: application/json", respGETService.getEntity().getContentType().toString());
+	}
+	
+	@Test(priority = 4, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_Swapi_GET_ID_TEST_OO4() throws ClientProtocolException, IOException, JSONException {
+		
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+				+ "--------------------" + "\n", reporterFlag);
+		
+		respCode = getServiceStatusCode("http://swapi.co/api/planets/1/");
+		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag);
+		Assert.assertEquals(respCode, HttpStatus.SC_OK);
+		
+		respGETService = getServiceContent();
+		String strResp = EntityUtils.toString(respGETService.getEntity());
+		JSONObject jo = new JSONObject(strResp);
+		
+		Assert.assertEquals(jo.getString("name"), "Tatooine");
+		Assert.assertEquals(jo.getString("rotation_period"), "23");
+		Assert.assertEquals(jo.getString("orbital_period"), "304");
+		Assert.assertEquals(jo.getString("diameter"), "10465");
+		Assert.assertEquals(jo.getString("climate"), "arid");
+		Assert.assertEquals(jo.getString("gravity"), "1 standard");
+		Assert.assertEquals(jo.getString("terrain"), "desert");
+		Assert.assertEquals(jo.getString("surface_water"), "1");
+		Assert.assertEquals(jo.getString("population"), "200000");
+		Assert.assertEquals(jo.getString("created"), "2014-12-09T13:50:49.641000Z");
+		Assert.assertEquals(jo.getString("edited"), "2014-12-21T20:48:04.175778Z");
+		Assert.assertEquals(jo.getString("url"), "http://swapi.co/api/planets/1/");
+		
+		String [] expRes = {
+				"http://swapi.co/api/people/1/",
+		        "http://swapi.co/api/people/2/", 
+		        "http://swapi.co/api/people/4/", 
+		        "http://swapi.co/api/people/6/", 
+		        "http://swapi.co/api/people/7/", 
+		        "http://swapi.co/api/people/8/", 
+		        "http://swapi.co/api/people/9/", 
+		        "http://swapi.co/api/people/11/", 
+		        "http://swapi.co/api/people/43/", 
+		        "http://swapi.co/api/people/62/"
+		};
+		
+		JSONArray jaRes = jo.getJSONArray("residents");
+		int iLengthTmp = jaRes.toString().length();
+		String[] strRes = jaRes.toString().toString().substring(1, iLengthTmp - 1).split(",");
+		
+		for(int i=0; i<strRes.length; i++) {
+			Assert.assertEquals(strRes[i].replaceAll("\"", ""), expRes[i]);
+		};
+		
+		String [] expFilms = {
+				"http://swapi.co/api/films/5/", 
+		        "http://swapi.co/api/films/4/", 
+		        "http://swapi.co/api/films/6/", 
+		        "http://swapi.co/api/films/3/", 
+		        "http://swapi.co/api/films/1/"
+		};
+		
+		JSONArray jaFilms = jo.getJSONArray("films");
+		iLengthTmp = jaFilms.toString().length();
+		String[] strFilms = jaFilms.toString().toString().substring(1, iLengthTmp - 1).split(",");
+		for(int i=0; i<strFilms.length; i++) {
+			Assert.assertEquals(strFilms[i].replaceAll("\"", ""), expFilms[i]);
+		}
+	}
+	
+	@Test(priority=5, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_SWAPI_GET_ID_Test_005() throws ClientProtocolException, IOException {
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+		+ "--------------------" + "\n", reporterFlag);
+		
+		respCode = getServiceStatusCode("http://swapi.co/api/planets/1/?search=too");
+		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag); 
+		Assert.assertEquals(respCode, HttpStatus.SC_OK);
+		
+	}
+	
+	@Test(priority=6, dependsOnMethods = "Auto_SWAPI_GET_Base_Test_001")
+	public static void Auto_SWAPI_GET_ID_Test_006() throws ClientProtocolException, IOException {
+		Reporter.log("\n" + "--------------------Start of test: " + Task1_Get_All_Planets.getCurrentMethodName()
+		+ "--------------------" + "\n", reporterFlag);
+		
+		respCode = getServiceStatusCode("http://swapi.co/api/planets/1/?search=toy");
 		Reporter.log("HttpResponse Status Code: " + respCode, reporterFlag); 
 		Assert.assertEquals(respCode, HttpStatus.SC_NOT_FOUND);
+		
 	}
 }
